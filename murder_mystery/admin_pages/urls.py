e1de8c5b1c6b3e9f66cf09da01cfe0e68a625d01
Dalton Cole <dalton.cole1@gmail.com>
2024-10-19 21:04:16 -0600

    Add test-console to admin page

--- murder_mystery/admin_pages/urls.py
+++ murder_mystery/admin_pages/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'admin-pages'
 urlpatterns = [
     path('console', views.console, name='console'),
+    path('test-console', views.test_console, name='test-console'),
 ]
--- murder_mystery/admin_pages/views.py
+++ murder_mystery/admin_pages/views.py
@@ -3,6 +3,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 from admin_pages.scripts.start_game import start_game
 from admin_pages.scripts.make_teams import make_teams
+from admin_pages.tests.helpers import make_n_users_and_characters, save_all
 
 
 # Create your views here.
@@ -15,11 +16,21 @@ def console(request):
             context['action'] = 'Game has been started'
             context['action'] = start_game()
 
-        if 'team-creation' == request.POST['action']:
-            # TODO: make f-string include number of teams created and what
-            # teams are. Include solo team statistics
-            context['action'] = 'Teams were created'
-            context['action'] = make_teams()
-
-
     return render(request, 'admin_pages/console.html', context)
+
+@staff_member_required
+def test_console(request):
+    context = {}
+
+    if 'action' in request.POST.keys():
+        if 'start-game' == request.POST['action']:
+            context['action'] = 'Game has been started'
+            context['action'] = start_game()
+
+        if 'add-default-character' == request.POST['action']:
+            user, char = make_n_users_and_characters(1)
+            save_all(user)
+            save_all(char)
+            context['action'] = 'Added 1 user'
+
+    return render(request, 'admin_pages/test-console.html', context)
