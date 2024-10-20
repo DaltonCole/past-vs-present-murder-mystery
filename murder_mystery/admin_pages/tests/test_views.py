31537d02303cf230c59ad15ed1f76def49adb20b
Dalton Cole <dalton.cole1@gmail.com>
2024-10-19 21:43:02 -0600

    Add tests for admin console

--- murder_mystery/admin_pages/tests/test_views.py
+++ murder_mystery/admin_pages/tests/test_views.py
@@ -6,37 +6,45 @@ import logging
 from characters.models import Character
 from teams.models import Team
 from .helpers import make_admin_user, make_n_users_and_characters, save_all
+from .test_scripts import TeamToClueEndState
 
 
 # Create your tests here.
 logging.basicConfig(level=logging.INFO)
 
-class ViewsTests(TestCase):
+class ConsoleTests(TeamToClueEndState):
+    fixtures = ['fixtures/descriptor_flavor_text.json',
+                'fixtures/occupation_flavor_text.json',
+                'fixtures/story_text_clue.json',
+                'fixtures/video_clues.json',
+                ]
+
     def setUp(self):
         self.user = make_admin_user()
         self.client.force_login(user=self.user)
+        self.reverse = 'admin-pages:console'
 
     def test_start_game(self):
-        # TODO
-        with self.assertLogs(level='INFO') as lc:
-            response = self.client.post(reverse('admin-pages:console'), {'action': 'start-game'}, follow=True)
-        # Check status code
-        self.assertEqual(response.status_code, 200)
-
-    def test_console_get_team_creation(self):
-        users, chars = make_n_users_and_characters(11)
+        users, chars = make_n_users_and_characters(5)
         save_all(users)
         save_all(chars)
-
         with self.assertLogs(level='INFO') as lc:
-            response = self.client.post(reverse('admin-pages:console'), {'action': 'team-creation'}, follow=True)
-            # Check status code
-            self.assertEqual(response.status_code, 200)
+            response = self.client.post(reverse(self.reverse), {'action': 'start-game'}, follow=True)
+        # Check status code
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(Team.objects.all()), 3)
+        self.check_team_to_clue_end_state()
 
-            # Check logs
-            self.assertIn('to a team as the past character', '\t'.join(lc.output)) # 1 solo
-            self.assertIn('from past to future', '\t'.join(lc.output)) # Change time for chars
 
-        # Verify 6 teams (5 duo, 1 single)
-        self.assertEqual(11, len(Character.objects.all()))
-        self.assertEqual(6, len(Team.objects.all()))
+class TestConsoleTests(ConsoleTests):
+    def setUp(self):
+        super().setUp()
+        self.reverse = 'admin-pages:test-console'
+
+    def test_add_default_character(self):
+        num_users = len(User.objects.all())
+        num_chars = len(Character.objects.all())
+        response = self.client.post(reverse(self.reverse), {'action': 'add-default-character'}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(User.objects.all()), num_users + 1)
+        self.assertEqual(len(Character.objects.all()), num_chars + 1)
--- murder_mystery/admin_pages/views.py
+++ murder_mystery/admin_pages/views.py
@@ -5,21 +5,8 @@ from admin_pages.scripts.start_game import start_game
 from admin_pages.scripts.make_teams import make_teams
 from admin_pages.tests.helpers import make_n_users_and_characters, save_all
 
-
-# Create your views here.
-@staff_member_required
-def console(request):
-    context = {}
-
-    if 'action' in request.POST.keys():
-        if 'start-game' == request.POST['action']:
-            context['action'] = 'Game has been started'
-            context['action'] = start_game()
-
-    return render(request, 'admin_pages/console.html', context)
-
-@staff_member_required
-def test_console(request):
+def action(request):
+    '''Helper function for handling the "action" POST request'''
     context = {}
 
     if 'action' in request.POST.keys():
@@ -32,5 +19,18 @@ def test_console(request):
             save_all(user)
             save_all(char)
             context['action'] = 'Added 1 user'
+
+    return context
+
+# Create your views here.
+@staff_member_required
+def console(request):
+    context = action(request)
+
+    return render(request, 'admin_pages/console.html', context)
+
+@staff_member_required
+def test_console(request):
+    context = action(request)
 
     return render(request, 'admin_pages/test-console.html', context)
