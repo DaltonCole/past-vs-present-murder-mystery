4829183d46357e1bc9f6a9141be1a0e5fb57a000
Dalton Cole <dalton.cole1@gmail.com>
2024-10-19 18:55:29 -0600

    Add start_game() call on character creation if teams exist (i.e. game has already started (#6)

--- murder_mystery/admin_pages/scripts/start_game.py
+++ murder_mystery/admin_pages/scripts/start_game.py
@@ -1,4 +1,11 @@
 
 
+# TODO: This should work even if it is called multiple times
 def start_game():
     return 'game has started'
+
+
+# End the game, do not allow any changes to the Database
+def end_game():
+    pass
+
