0b401436ad2a558ea989d351a27a620f2a748dd4
Dalton Cole <dalton.cole1@gmail.com>
2024-10-20 17:43:36 -0600

    Added context to home page

---
+++ murder_mystery/pages/tests/test_views.py
@@ -0,0 +1,80 @@
+import logging
+
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+from django.test import TestCase
+
+from admin_pages.scripts.make_teams import make_teams
+from admin_pages.tests.helpers import (
+    make_admin_user,
+    make_n_users_and_characters,
+    save_all,
+)
+from admin_pages.tests.test_scripts import MakeTeams
+from characters.models import Character
+from teams.models import Team
+
+
+# Create your tests here.
+logging.basicConfig(level=logging.INFO)
+
+ALL_FIXTURES = ['fixtures/bonus_points.json',
+                'fixtures/descriptor_flavor_text.json',
+                'fixtures/occupation_flavor_text.json',
+                'fixtures/story_text_clue.json',
+                'fixtures/video_clues.json',
+                ]
+
+class HomePreGameTests(TestCase):
+    fixtures = ALL_FIXTURES
+
+    def setUp(self):
+        self.reverse = reverse('pages:home')
+
+    def test_signup_page_redirect(self):
+        '''Assert if not logged in, go to signup page'''
+        response = self.client.get(self.reverse, follow=True)
+        self.assertRedirects(response, '/accounts/signup/?next=%2F')
+
+    def test_character_creation_redirect(self):
+        '''Assert user with no character redirects to character creation screen'''
+        user = User(username=1, password='')
+        user.save()
+        self.client.force_login(user=user)
+        response = self.client.get(self.reverse, follow=True)
+        self.assertRedirects(response, reverse('characters:creation'))
+
+    def test_waiting_for_game(self):
+        '''If user has a character but no team, they should see the waiting for game to start page'''
+        user = User(username=1, password='')
+        user.save()
+        self.client.force_login(user=user)
+        char = Character(username=user, solo=False)
+        char.save()
+        self.assertEqual(len(Team.objects.all()), 0)
+        response = self.client.get(self.reverse, follow=True)
+        self.assertTemplateUsed(response, 'pages/game-has-not-started.html')
+
+
+class HomeDuringGameTests(MakeTeams):
+    fixtures = ALL_FIXTURES
+
+    def setUp(self):
+        self.make_teams()
+        self.user = User.objects.all()[0]
+        self.char = Character.objects.get(username=self.user)
+        self.reverse = reverse('pages:home')
+        self.client.force_login(user=self.user)
+
+    def tearDown(self):
+        self.assertTemplateNotUsed(self.response, 'pages/game-has-not-started.html')
+
+    def test_context(self):
+        '''Verify context'''
+        # TODO
+        with self.assertLogs(level='INFO') as lc:
+            self.response = self.client.post(self.reverse, {'action': 'start-game'}, follow=True)
+        # Check status code
+        self.assertEqual(self.response.status_code, 200)
+
+
--- murder_mystery/pages/views.py
+++ murder_mystery/pages/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from characters.models import Character
+from pages.scripts.calculate_team_score import calculate_team_score
+from teams.models import Team
+from teams.scripts.get_next_clue import get_next_clue
+from teams.scripts.get_solved_clues import get_solved_clues
+from teams.scripts.get_team import get_team
 
 @login_required(login_url='accounts/signup')
 def home(request):
+    context = {}
     # Go to character creation screen if no character exists for this user
     try:
-        matching_character = Character.objects.get(username=request.user.id)
+        context['character'] = Character.objects.get(username=request.user.id)
     except:
         return redirect('characters:creation')
 
-    context = {
-        'matching_character': matching_character,
-    }
+    # If the game has not started yet, go to the waiting page
+    if len(Team.objects.all()) == 0:
+        return render(request, 'pages/game-has-not-started.html', context)
+
+    # Get team
+    context['team'] = get_team(context['character'])
+
+    # Get score
+    context['total_points'], context['points_reason'] = calculate_team_score(context['team'])
+
+    # Get solved clues
+    context['solved_clues'] = get_solved_clues(context['team'])
+
+    # Get next clue
+    context['next_clue'] = get_next_clue(context['team'])
 
     return render(request, 'home.html', context)
-
