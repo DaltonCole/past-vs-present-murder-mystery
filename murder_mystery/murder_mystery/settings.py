15979e91ebae1e75a3186e3144c613cd4b60eb2b
Dalton Russell Cole <dalton.cole1@gmail.com>
2024-10-26 15:06:04 -0600

    Moved dev compose file down a directory

--- murder_mystery/murder_mystery/settings.py
+++ murder_mystery/murder_mystery/settings.py
@@ -30,7 +30,8 @@ DEBUG = os.environ.get('DEBUG', default=False)
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost 127.0.0.1 [::1] 0.0.0.0").split(" ")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # Application definition
 
